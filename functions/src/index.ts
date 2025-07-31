0e753b1737b2b30ae65ea2cabcd3eeb8a2474d4e
James Riggall <jnriggall@gmail.com>
2025-07-31 18:04:22 +0100

    Update PDF generator, Firebase options, and universal link config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,6 @@ import './firebase.js';
 
 export { extractAndFormatRecipe } from './extractAndFormatRecipe.js';
 export { deleteAccount } from './delete_account.js';
-export { sharedRecipePage } from './shared_recipe_page.js';
 export { seedDefaultRecipes } from './seed_default_recipes.js';
 export { copyGlobalRecipesToUser } from './copy_global_recipes.js';
 export { refreshGlobalRecipesForUser } from './refresh_global_recipes.js';
